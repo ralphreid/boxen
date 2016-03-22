742b6271070f946997f4e34a0267daef8be53022
Ralph Reid <beresfordjunior@me.com>
2016-03-22 12:36:56 -0700

    Added vim configs

--- modules/profile/manifests/vim.pp
+++ modules/profile/manifests/vim.pp
@@ -6,4 +6,28 @@ class profile::vim {
     content => template('profile/vimrc.erb'),
   }
 
+  file { "ftdetect":
+    ensure  => file,
+    path    => "/Users/${::boxen_user}/.vim/ftdetect.vim",
+    content => template('profile/vim/ftdetect.vim.erb'),
+  }
+
+  file { "ftplugin":
+    ensure  => file,
+    path    => "/Users/${::boxen_user}/.vim/ftplugin.vim",
+    content => template('profile/vim/ftplugin.vim.erb'),
+  }
+
+  file { "indent":
+    ensure  => file,
+    path    => "/Users/${::boxen_user}/.vim/indent.vim",
+    content => template('profile/vim/indent.vim.erb'),
+  }
+
+  file { "syntax":
+    ensure  => file,
+    path    => "/Users/${::boxen_user}/.vim/syntax.vim",
+    content => template('profile/vim/syntax.vim.erb'),
+  }
+
 }
