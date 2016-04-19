e7ff46472ebd7596e94c9590c5ec5ac9afc18327
Ralph Reid <beresfordjunior@me.com>
2016-04-18 22:32:48 -0700

    Added battery tap

--- modules/profile/manifests/tmux.pp
+++ modules/profile/manifests/tmux.pp
@@ -3,10 +3,13 @@ class profile::tmux {
   package { 'tmux': }
 
   # tmux configuration
-  file { "tmux.conf":
+  file { 'tmux.conf':
     ensure  => file,
     path    => "/Users/${::boxen_user}/.tmux.conf",
     content => template('profile/tmux.conf.erb'),
   }
+
+  # Installs battery for right-status
+  homebrew::tap { 'goles/battery': }
 
 }
