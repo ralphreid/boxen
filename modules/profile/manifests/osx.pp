ea65fd6f6cdb603de08231eadfad37c40f0ef45b
Ralph Reid <beresfordjunior@me.com>
2016-08-10 22:45:57 -0700

    added osx setting

--- modules/profile/manifests/osx.pp
+++ modules/profile/manifests/osx.pp
@@ -4,7 +4,8 @@ class profile::osx {
 
   # Dock Settings
   include osx::dock::autohide
-  class { 'osx::dock::position':
-    position => 'left'
-  }
+  class { 'osx::dock::position': position => 'left' }
+  # Public: Enable one or two button mode for multitouch mice (default = 1).
+  # Requires re-login.
+  class { 'osx::mouse::button_mode': mode => 2 }
 }
